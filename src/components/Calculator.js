1649811937a4157f2b588a7ca9c7cf01f4164017
Carly Ciricillo <carly.ciricillo@wunderkind.co>
2022-10-11 22:08:51 -0400

    Incorporate feedback

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,21 +8,12 @@ const Calculator = () => {
   /** TODO: Here is where you are going to keep track of calculator state */
   const [count, setCount] = useState(0);
   const [displayCount, setDisplayCount] = useState(0);
-  var operator;
+  const [operator, setOperator] = useState('');
 
   /** TODO: what happens when I click a number? */
-  const handleNumberClick = (value) => {
-    if (operator === "+") {
-      setCount(parseInt(count) + value);
-      setDisplayCount(value);
-    } else if (operator === "-") {
-      setCount(parseInt(count) - value);
-      setDisplayCount(value);
-    } else if (operator === "x") {
-      setCount(parseInt(count) * value);
-      setDisplayCount(value);
-    } else if (operator === "/") {
-      setCount(parseInt(count) / value);
+  const handleNumberClick = value => {
+    if (operator) {
+      setCount(eval(`parseInt(count) ${operator} value`));
       setDisplayCount(value);
     } else {
       if (parseInt(count) === 0) {
@@ -36,45 +27,38 @@ const Calculator = () => {
   };
 
   /** TODO: what happens when I click an operation? */
-  const handleOperationClick = (value) => {
+  const handleOperationClick = value => {
     if (value === "clear") {
       setCount(0);
       setDisplayCount(0);
+      setOperator('');
     } else if (value === "=") {
       setDisplayCount(count);
     } else {
-      operator = value;
+      setOperator(value);
     }
   };
 
+  const makeKeys = (type,[a,b,c,d,e]) => {
+    let keys;
+    if (type=="number") {
+      keys = [a,b,c,d,e].map(n => <Number key={n} value={n} onClick={handleNumberClick} />);
+    } else {
+      keys = [a,b,c,d,e].map(n => <Operation key={n} value={n} onClick={handleOperationClick} />);
+    };
+    return <div style={{ flexBasis: "33%", flexGrow: "1" }}>{keys}</div>
+  };
+  
   return (
     <div style={{ border: "5px solid #02040F", borderRadius: "10px", padding: "0.7rem", backgroundColor: "#f2f2f2",}}>
       <Screen value={displayCount} />
       <div style={{ display: "flex", gap: "0.6rem" }}>
-        <div style={{ flexBasis: "33%", flexGrow: "1" }}>
-          <Number value={0} onClick={handleNumberClick} />
-          <Number value={1} onClick={handleNumberClick} />
-          <Number value={2} onClick={handleNumberClick} />
-          <Number value={3} onClick={handleNumberClick} />
-          <Number value={4} onClick={handleNumberClick} />
-        </div>
-        <div style={{ flexBasis: "33%", flexGrow: "1" }}>
-          <Number value={5} onClick={handleNumberClick} />
-          <Number value={6} onClick={handleNumberClick} />
-          <Number value={7} onClick={handleNumberClick} />
-          <Number value={8} onClick={handleNumberClick} />
-          <Number value={9} onClick={handleNumberClick} />
-        </div>
-        <div style={{ flexBasis: "33%", flexGrow: "1" }}>
-          <Operation value="+" onClick={handleOperationClick} />
-          <Operation value="-" onClick={handleOperationClick} />
-          <Operation value="x" onClick={handleOperationClick} />
-          <Operation value="/" onClick={handleOperationClick} />
-          <Operation value="=" onClick={handleOperationClick} />
-        </div>
+        {makeKeys("number", [0,1,2,3,4])}
+        {makeKeys("number", [5,6,7,8,9])}
+        {makeKeys("operation", ['+','-','*','/','='])}
       </div>
       <div>
-          <Operation value="clear" onClick={handleOperationClick} />
+        {makeKeys("operation", ['clear'])}
       </div>
     </div>
   );
